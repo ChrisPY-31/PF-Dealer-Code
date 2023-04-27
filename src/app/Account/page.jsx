dc721e5cb762cdd5d1e689ab391febb61ba33fd6
evinlopez90 <evinlopez90@gmail.com>
2023-04-27 14:57:00 -0500

    Configuracion

--- src/app/Account/page.jsx
+++ src/app/Account/page.jsx
@@ -1,10 +1,10 @@
-import React from 'react'
 
+// import NavUser from "../../Comonents/NavUser/NavUser"
 const Page = () => {
   return (
-    <div className=''>
-    Hola  
-    </div>
+    <>
+
+    </>
   )
 }
 
