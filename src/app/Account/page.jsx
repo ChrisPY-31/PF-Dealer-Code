c20eb1720c6a8eae491382a9132b4e671f4c0761
Christian <christianpeya@hotmail.com>
2023-04-26 19:02:56 -0600

    Enrutado

---
+++ src/app/Account/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Page = () => {
+  return (
+    <div>Desde cuenta del user</div>
+  )
+}
+
+export default Page
---
+++ src/app/Buy-Course/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Page = () => {
+  return (
+    <div>Desde Comprar curso</div>
+  )
+}
+
+export default Page
