5eaced04229a8cbb43358565ba145cd0316b67f2
ferchu <fernandojosefantini7.0@gmail.com>
2023-04-28 10:55:44 -0300

    Nav y home cambios

---
+++ components/Nav/NavBar.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import Link from 'next/link'
+import {RxMagnifyingGlass} from "react-icons/rx"
+import {BsCart4} from "react-icons/bs"
+import {BsPersonCircle} from "react-icons/bs"
+import {MdOutlineKeyboardArrowDown} from"react-icons/md"
+export default function NavBar() {
+  return (
+
+    <div className=" bg-tarawera-700 flex flex-wrap items-center justify-around ">
+        <h1>Logo</h1>
+        <div className="flex ">
+            <Link href="/course">
+            <ul className="mr-5 hover:underline  decoration-white">Explorar Cursos</ul>
+            </Link>
+            <Link href="/Favorits">
+            <ul className="hover:underline decoration-white">Favoritos</ul>
+            </Link>
+        </div>
+        <div className="flex flex-wrap items-center">
+            <Link href="/Home">
+        <h1 className="bg-green-400  pl-3 pr-3 rounded-md hover:bg-green-500">Inicio</h1>
+        </Link>
+           <div className="flex flex-wrap items-center p-2 ml-5 m-2 rounded-xl bg-tarawera-500">
+            <button className="  p-1 mr-2 top-5 text-gray-950 rounded-full"><RxMagnifyingGlass/></button>
+            <input placeholder='¿Que buscas aprender?' type="text" className="p-1 placeholder-black bg-transparent"></input>
+          </div>
+        </div>
+        <div className="flex flex-wrap items-center">
+            <button className='mr-5 p-1 rounded-xl hover:bg-tarawera-600'><BsCart4/></button>
+            <Link href="/Account">
+            <div className="flex flex-wrap items-center p-2 rounded-xl bg-green-600 hover:bg-green-700"><p className='mr-1'><MdOutlineKeyboardArrowDown/></p><BsPersonCircle/></div>
+            </Link>
+        </div>
+    </div>
+  )
+}
+/* lupa=RxMagnifyingGlass
+carrito=BsCart4
+usuario=BsPersonCircle */
--- src/app/Home/page.jsx
+++ src/app/Home/page.jsx
@@ -1,8 +1,33 @@
 import React from 'react'
-
+import NavBar from '../../../components/Nav/NavBar'
 const Page = () => {
   return (
-    <div>Desde Home"</div>
+    <div>
+  <NavBar></NavBar>
+  <div>
+    <div>
+      <h1 className="m-5">Curso Destacado</h1>
+        <div className='flex items-center hover:bg-slate-100' style={{margin:"15px", border:"1px", borderStyle:"solid", borderColor:"grey"}}>
+          <div className="m-5 p-28 pl-36 pr-36" style={{border:"1px", borderStyle:"solid", borderColor:"grey"}}>
+            <h1>Imagen</h1>
+            </div>
+        <div>
+         <h1>Nombre de curso</h1>
+           <h2>Sub titulo</h2>
+            <p>Valoracion</p>
+            <p className=" mt-24">Precio</p>
+            <div><button className="mr-2 mt-5">Comprar</button></div>
+        </div>
+       </div>
+    </div>
+  <div>
+<h1>Mis Cursos</h1>
+</div>
+<div>
+  <h1>Recomendaciones</h1>
+</div>
+  </div>
+</div>
   )
 }
 
