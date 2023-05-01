55baa5db2a3c638562517667d1559ea8e26b047b
ChrisDev <Christianpeya@hotmail.com>
2023-05-01 13:15:57 -0600

    terminado

---
+++ src/app/layout.js
@@ -0,0 +1,23 @@
+'use client'
+import { store } from "@/store";
+import "./globals.css";
+import { Inter } from "next/font/google";
+import React from "react";
+import { Provider } from "react-redux";
+const inter = Inter({ subsets: ["latin"] });
+
+
+export default function RootLayout({ children }) {
+  return (
+    <Provider store={store}>
+      <html lang="en">
+        <body
+          className={inter.className}
+          style={{ backgroundColor: "#053545", color: "white" }}
+        >
+          {children}
+        </body>
+      </html>
+    </Provider>
+  );
+}
