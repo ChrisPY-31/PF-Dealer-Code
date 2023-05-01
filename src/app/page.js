4a6a83ee53f7a2ef1ec673cfdecd0e572700be50
ChrisDev <Christianpeya@hotmail.com>
2023-05-01 13:06:15 -0600

    modificado page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,9 @@
-import Menu from "@/Comonents/Menu/Menu";
+
 
 export default function Home() {
   return (
     <main className="">
-<<<<<<< HEAD
-    Hola
-=======
       <h1>Hola amigos</h1>
->>>>>>> 70ae0d3d827eb265a29cf2d88d5baa5b287d69ee
     </main>
   )
 }
