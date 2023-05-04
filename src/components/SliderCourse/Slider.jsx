1dc352abcc0419c66edddeb8827b8f70f659bcfe
ChrisDev <Christianpeya@hotmail.com>
2023-05-03 21:32:57 -0600

    Slider terminado y formularios

--- src/components/SliderCourse/Slider.jsx
+++ src/components/SliderCourse/Slider.jsx
@@ -1,48 +1,101 @@
-'use client'
-import React from 'react'
-// import './Slider.css'
-// import {Swiper , SwiperSlider } from "swiper/react"
-// import 'swiper/css'
-// import 'swiper/css/effect-coverflow'
-// import 'swiper/css/pagination'
-// import 'swiper/css/navigation'
+"use client";
+import React from "react";
+import "./Slider.css";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
 
-// import { EffectCoverflow , Pagination , Navigation } from 'swiper'
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/effect-coverflow";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+
+// import required modules
+import { EffectCoverflow, Pagination, Navigation } from "swiper";
+
 const Slider = () => {
   return (
-    <div className='flex justify-center'>
-      {/* <h1>Cursos que encontraras en Dealer Code</h1>
+    <div className="container">
+      {/* <h2 className="heading text-2xl font-medium">Todo lo que puedes aprender</h2> */}
       <Swiper
-      effect={'coverflow'}
-      grabCursor={true}
-      centeredSlides={true}
-      loop={true}
-      slidesPerView={ 'auto'}
-      coverflowEffect={
-        {
-          rotate:0,
-          stretch:0,
-          depth:100,
-          modifier: 2.5
-        }
-      }  
-            
-            >
-        <SwiperSlider>
-          <img src="https://cdn-images-1.medium.com/max/280/1*lKN9xV1YEin-2wfAiGySBA.png" alt="" />
-        </SwiperSlider>
-        <SwiperSlider>
-          <img src="https://p.kindpng.com/picc/s/171-1718046_javascript-programming-language-logo-hd-png-download.png" alt="" />
-        </SwiperSlider>
-        <SwiperSlider>
-          <img src="https://cdn-images-1.medium.com/max/280/1*lKN9xV1YEin-2wfAiGySBA.png" alt="" />
-        </SwiperSlider>
-        <SwiperSlider>
-          <img src="https://p.kindpng.com/picc/s/171-1718046_javascript-programming-language-logo-hd-png-download.png" alt="" />
-        </SwiperSlider>
-      </Swiper> */}
-    </div>
-  )
-}
+        effect={"coverflow"}
+        grabCursor={true}
+        centeredSlides={true}
+        loop={true}
+        slidesPerView={"auto"}
+        coverflowEffect={{
+          rotate: 0,
+          stretch: 200,
+          depth: 120,
+          modifier: 2.5,
+        }}
+        pagination={{ el: ".swiper-pagination", clickable: true }}
+        navigation={{
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+          clickable: true,
+        }}
+        modules={[EffectCoverflow, Pagination, Navigation]}
+        className="swiper_container"
+      >
+        <SwiperSlide>
+          <div className="flex justify-center">
+            <img src="https://swiperjs.com/demos/images/nature-1.jpg" />
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+        <div className="flex justify-center">
+            <img src="https://swiperjs.com/demos/images/nature-2.jpg" />
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+        <div className="flex justify-center">
+            <img src="https://swiperjs.com/demos/images/nature-3.jpg" />
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+        <div className="flex justify-center">
+            <img src="https://swiperjs.com/demos/images/nature-4.jpg" />
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+        <div className="flex justify-center">
+            <img src="https://swiperjs.com/demos/images/nature-5.jpg" />
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+        <div className="flex justify-center">
+            <img src="https://swiperjs.com/demos/images/nature-6.jpg" />
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+        <div className="flex justify-center">
+            <img src="https://swiperjs.com/demos/images/nature-7.jpg" />
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+        <div className="flex justify-center">
+            <img src="https://swiperjs.com/demos/images/nature-8.jpg" />
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="flex justify-center">
+            <img src="https://swiperjs.com/demos/images/nature-9.jpg" />
+          </div>
+        </SwiperSlide>
 
-export default Slider
+        {/* <div className="slider-controler">
+          <div className="swiper-button-prev slider-arrow">
+            <ion-icon name="arrow-back-outline"></ion-icon>
+          </div>
+          <div className="swiper-button-next slider-arrow">
+            <ion-icon name="arrow-forward-outline"></ion-icon>
+          </div>
+          <div className="swiper-pagination"></div>
+        </div> */}
+      </Swiper>
+    </div>
+  );
+};
+
+export default Slider;
