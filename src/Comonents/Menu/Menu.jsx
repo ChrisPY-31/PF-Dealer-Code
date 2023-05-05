14f24b536d53d2a031cb1d056e0f9a76a6cdc64f
ChrisDev <Christianpeya@hotmail.com>
2023-05-05 14:42:42 -0600

    cursos pointer al cerrar sesion

--- src/Comonents/Menu/Menu.jsx
+++ src/Comonents/Menu/Menu.jsx
@@ -55,7 +55,7 @@ function Menu() {
             <hr className="border-gray-200 dark:border-gray-700 "></hr>
             
             
-            <p href="#" onClick={handleClick} className="block px-4 py-3 text-sm text-gray-600 capitalize transition-colors duration-200 transform dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-700 dark:hover:text-white">
+            <p href="#" onClick={handleClick} className="block px-4 py-3 text-sm text-gray-600 capitalize transition-colors duration-200 transform dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-700 dark:hover:text-white cursor-pointer">
                 Cerrar sesion 
             </p>
         </div> : null
