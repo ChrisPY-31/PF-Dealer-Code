4ec89d461b7b6553066acfe591757545f9ce9bd1
ChrisDev <Christianpeya@hotmail.com>
2023-05-08 10:50:57 -0600

    video integrado

--- src/components/Video/Video.jsx
+++ src/components/Video/Video.jsx
@@ -1,17 +1,37 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 
 const Video = () => {
+  const [userValidation, setUserValidation] = useState(false);
   return (
     <div className="bg-white h-5/6 w-11/12 rounded-3xl">
       <div className="text-black flex flex-col justify-evenly items-center h-full">
-        <img
-          className="w-64 h-64"
-          src="https://codigofacilito.com/assets/svg/upgrade-a945c2f2382a2f704fb10f48be425150fd37294f23dd7f68227a7c4efc4b4fb6.svg"
-          alt=""
-        />
-        <h3>¡Desbloquea vídeos ilimitados! Sube a Premium</h3>
-        <p>Necesitas premium para acceder a este contenido. Suscríbete por $480MXN al mes</p>
-        <button className="py-4 px-10 bg-yellow-400 text-white rounded-md">Comprar el curso</button>
+        {userValidation ? (
+          <video
+            src="https://www.youtube.com/watch?v=DFehUVIFoa8.ogm"
+            controls
+            autoplay
+            poster="https://upload.wikimedia.org/wikipedia/commons/1/18/React_Native_Logo.png"
+          >
+            Tu navegador no admite el elemento <code>video</code>.
+          </video>
+        ) : (
+          <div className="text-black flex flex-col justify-evenly items-center h-full">
+            <img
+              className="w-64 h-64"
+              src="https://codigofacilito.com/assets/svg/upgrade-a945c2f2382a2f704fb10f48be425150fd37294f23dd7f68227a7c4efc4b4fb6.svg"
+              alt=""
+            />
+            <h3>¡Desbloquea vídeos ilimitados! Sube a Premium</h3>
+            <p>
+              Necesitas premium para acceder a este contenido. Suscríbete por
+              $480MXN al mes
+            </p>
+            <button className="py-4 px-10 bg-yellow-400 text-white rounded-md">
+              Comprar el curso
+            </button>
+          </div>
+        )}
       </div>
     </div>
   );
