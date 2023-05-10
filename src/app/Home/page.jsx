561d7c2b284730feaf18b46568211784d2d611a0
ChrisDev <Christianpeya@hotmail.com>
2023-05-10 12:21:03 -0600

    formulario en formik validaciones etc

--- src/app/Home/page.jsx
+++ src/app/Home/page.jsx
@@ -1,66 +1,80 @@
-"use client"
-import React,{useEffect, useState} from 'react'
-import NavBar from './components/Nav/NavBar'
-import { useSelector } from 'react-redux'
-import { getCourses} from "../../store/reducer/course"
+"use client";
+import React, { useEffect, useState } from "react";
+import NavBar from "./components/Nav/NavBar";
+import { useSelector } from "react-redux";
+import { getCourses } from "../../store/reducer/course";
 import { useDispatch } from "react-redux";
-import { DB } from './db'
-import Link from 'next/link'
-import CardP from './components/Cards/CardP'
-import CardR from './components/Cards/CardR'
+import { DB } from "./db";
+import Link from "next/link";
+import CardP from "./components/Cards/CardP";
+import CardR from "./components/Cards/CardR";
+import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer,} from "react-toastify";
 const Page = () => {
-  const [recomendaciones,setRecomendaciones]=useState("")
-  const dispatch = useDispatch()
-useEffect(() => {
-  dispatch(getCourses(DB))
-   }, []);
+  const [recomendaciones, setRecomendaciones] = useState("");
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getCourses(DB));
+  }, []);
   
-   
-   function Recomendaciones(){
-    var push=[]
-    if(Courses.length>3){
+  if(typeof document !== 'undefined') {
+    // you are safe to use the "document" object here
+    console.log(document.location.href);
+}
+  function Recomendaciones() {
+    var push = [];
+    if (Courses.length > 3) {
       for (let index = 0; index < 4; index++) {
-        let numeroA= Math.floor(Math.random() * Courses.length);
-        if(Courses[numeroA]!==undefined){
-          push.push(Courses[numeroA])
-      setRecomendaciones(push)
+        let numeroA = Math.floor(Math.random() * Courses.length);
+        if (Courses[numeroA] !== undefined) {
+          push.push(Courses[numeroA]);
+          setRecomendaciones(push);
         }
       }
     }
-     
   }
-  const Courses=useSelector(s=>s.course.courses)
-const numeroAleatorio = Math.floor(Math.random() * Courses.length);
-if(!recomendaciones){
-  Recomendaciones()
-}
-var db=Courses[numeroAleatorio]
- 
+  const Courses = useSelector((s) => s.course.courses);
+  const numeroAleatorio = Math.floor(Math.random() * Courses.length);
+  if (!recomendaciones) {
+    Recomendaciones();
+  }
+  var db = Courses[numeroAleatorio];
+
   return (
     <div>
-  <NavBar/>
-  <div>
-    <div>
-      <h1 className="mt-5 ml-5 text-2xl">Curso Destacado</h1>
-      <CardP Courses={Courses} db={db}></CardP>
+      <ToastContainer />
+      <NavBar />
+      <div>
+        <div>
+          <h1 className="mt-5 ml-5 text-2xl">Curso Destacado</h1>
+          <CardP Courses={Courses} db={db}></CardP>
+        </div>
+        <div className="flex">
+          <div className="ml-7 rounded-xl bg-slate-800 ">
+            <h1 className="text-2xl m-5 ">Mis Cursos</h1>
+            <Link href="/Home/Course">
+              <div
+                className="flex items-center p-16 m-5 flex-col hover:bg-slate-700"
+                style={{
+                  border: "1px",
+                  borderStyle: "solid",
+                  borderColor: "grey",
+                }}
+              >
+                <h1>Añadir</h1>
+              </div>
+            </Link>
+          </div>
+        </div>
+        <div>
+          <h1 className="text-2xl m-5">Recomendaciones</h1>
+          <div>
+            <CardR recomendaciones={recomendaciones}></CardR>
+          </div>
+        </div>
+      </div>
     </div>
-    <div className='flex'>
-  <div className='ml-7 rounded-xl bg-slate-800 '>
-<h1 className='text-2xl m-5 '>Mis Cursos</h1>
-        <Link href="/Home/Course"><div className="flex items-center p-16 m-5 flex-col hover:bg-slate-700" style={{border:"1px", borderStyle:"solid", borderColor:"grey"}}>
-  <h1>Añadir</h1>
-    </div></Link>
-  </div>
-</div>
-<div>
-  <h1 className='text-2xl m-5'>Recomendaciones</h1>
-   <div>
-   <CardR recomendaciones={recomendaciones}></CardR>
-  </div>
-</div>
-  </div>
-</div>
-  )
-}
+  );
+};
 
-export default Page
+export default Page;
--- src/store/reducer/thunk.js
+++ src/store/reducer/thunk.js
@@ -1,1 +1,13 @@
 //llamado de las api 
+
+import { dealerApi } from "@/api/api"
+
+//Conectamos el registro al api 
+export const getRegisterUser = (user) =>{
+    return async (dispatch , getState) =>{
+        console.log(user)
+        // const userRegister = await axios.Post(`${dealerApi}/user/signup`, user)
+        // console.log(userRegister)
+    }
+
+}
