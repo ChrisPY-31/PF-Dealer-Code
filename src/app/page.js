07bc502d2759c61ba8f06b915c35070c9c2ce3f4
evinlopez90 <evinlopez90@gmail.com>
2023-05-15 14:24:56 -0500

    correccion

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,5 @@
 "use client"
 import Landing from "@/components/Landing/Landing";
-<<<<<<< HEAD
-
-=======
->>>>>>> d6c440b7b307e27deee4e4c3bca93b975f57f491
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { getCursos } from "@/store/reducer/addPagos/agregarPago";
