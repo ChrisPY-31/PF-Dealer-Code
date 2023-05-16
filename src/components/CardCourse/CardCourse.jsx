51eff5f4dda45fcf6be750611b6e1d6b0ed6f1c7
ChrisDev <Christianpeya@hotmail.com>
2023-05-16 15:26:02 -0600

    detalles corregido

--- src/components/CardCourse/CardCourse.jsx
+++ src/components/CardCourse/CardCourse.jsx
@@ -6,13 +6,11 @@ import { FirebaseAuth } from "@/firebase/credenciales";
 import { onAuthStateChanged } from "firebase/auth";
 import { UseLocalStorage } from "@/Comonents/carrito/useLocalStorage";
 
+const CardCourse = ({ id, price, thumbnail, title }) => {
+  const [autentication, setAutentication] = useState(false);
 
+  let dispacth = useDispatch();
 
-
-const CardCourse = ({titulo, instructor,  categoria, precio, descripcion, id }) => {
-  const [autentication , setAutentication] = useState(false)
-  
-  let dispacth = useDispatch()
 
   let token;
   onAuthStateChanged(FirebaseAuth, (usuarioFirebase) => {
@@ -23,12 +21,12 @@ const CardCourse = ({titulo, instructor,  categoria, precio, descripcion, id }) 
     }
   });
 
-  
 
   let [producto, setProducto] = UseLocalStorage("producto", [])
 
   return (
     <div className="bg-indigo-600 h-full ">
+      <div className="">
         <div className="bg-teal-500 h-52 ">
           <video
             poster="https://blog.logrocket.com/wp-content/uploads/2020/08/8-ways-deploy-react-app-free.png"
@@ -41,52 +39,34 @@ const CardCourse = ({titulo, instructor,  categoria, precio, descripcion, id }) 
             />
           </video>
         </div>
-
         <div className="w-11/12 m-auto mt-10 ">
           <div>
             <h4 className="text-2xl">{title}</h4>
             <p className="my-1">
               <span className="text-lg">{price} MX</span>
             </p>
+
+            <p>{titulo}</p>
           </div>
-          <div>
+          <div >
             <div className="flex flex-col justify-between h-14">
               <div className="flex">
-                <Link
-                  href="checkaut/[id]"
-                  as={`/checkaut/${id}`}
-                  className={
-                    !autentication
-                      ? "py-2.5 w-full bg-lime-400 rounded-md text-center"
-                      : "w-64 py-2.5 bg-transparent rounded-md text-center"
-                  }
-                >
-                  {autentication ? (
-                    <h1 className="text-black"> ir al curso </h1>
-                  ) : (
-                    <h1 className="">Comprar curso</h1>
-                  )}
-                </Link>
-                {!autentication && (
-                  <Link href="/Favorits">
-                    <button className="px-4 py-2 bg-red-400 ml-4 rounded-md">
-                      Fav
-                    </button>
+                  <Link href="checkaut/[id]" as={`/checkaut/${id}`} className={!autentication?"py-2.5 w-full  rounded-md text-center": 'w-64 py-2.5 bg-transparent rounded-md text-center'}>
+                    {autentication ? <h1 className="text-black"> ir al curso </h1>: null }
+                    
                   </Link>
-                  {!autentication && 
-                    <Link href="/Favorits"><button className="px-4 py-2 bg-red-400 ml-4 rounded-md" >Fav</button></Link>
-                  }
+                  {!autentication && <div className="flex items-center">Añadir a Favoritos:<button className="p-2 bg-red-400 ml-4 rounded-md" onClick={()=>addFavorite(id)}>🤍</button></div> }
               </div>
             </div>
             {!autentication &&
-              <button onClick={
-                () => setProducto([ {
-                  titulo, instructor,  categoria, precio, idP: producto.length
-                } ])
-              } className="py-2.5 w-full bg-slate-600 rounded-lg">
-              Agregar a la Cesta
-            </button>
-              }
+            <button onClick={
+              () => setProducto([...producto, {
+                titulo, instructor,  precio, idP: producto.length, id
+              } ])
+            } className="py-2.5 w-full bg-slate-600 rounded-lg">
+            Agregar a la Cesta
+          </button>
+            }
           </div>
           <div>
             <div>
@@ -116,6 +96,8 @@ const CardCourse = ({titulo, instructor,  categoria, precio, descripcion, id }) 
               </div>
             </div>
           </div>
+        </div>
+      </div>
     </div>
   );
 };
