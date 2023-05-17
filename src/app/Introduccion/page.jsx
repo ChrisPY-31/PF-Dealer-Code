7738b697f2bb1b6545a7d14b06e28bcc13e9c07e
ChrisDev <Christianpeya@hotmail.com>
2023-05-17 14:54:36 -0600

    Videos cargados no terminado

--- src/app/Introduccion/page.jsx
+++ src/app/Introduccion/page.jsx
@@ -1,16 +1,19 @@
+'use client'
 import RutaVideo from "@/components/RutaVideo/RutaVideo";
 import Video from "@/components/Video/Video";
-import React from "react";
+import React, { useState } from "react";
 
 const page = () => {
+
+  const [videoId , setVideoId] = useState('')
   return (
     <div className="w-screen h-screen">
       <div className="flex h-5/6">
         <div className="bg-black w-2/3  flex justify-center items-center">
-            <Video/>
+            <Video videoId={videoId}/>
         </div>
         <div className="w-1/3 ">
-            <RutaVideo/>
+            <RutaVideo setVideoId={setVideoId}/>
         </div>
       </div>
     </div>
--- src/components/RutaCourse/RutaCourse.jsx
+++ src/components/RutaCourse/RutaCourse.jsx
@@ -1,12 +1,18 @@
-'use client'
-import React from 'react'
+"use client";
+import React from "react";
 import { Fragment, useState } from "react";
 import {
   Accordion,
   AccordionHeader,
   AccordionBody,
 } from "@material-tailwind/react";
- 
+import {
+  caracteristicas,
+  fundamentos,
+  introduccion,
+  piano,
+} from "@/app/Introduccion/videos";
+
 function Icon({ id, open }) {
   return (
     <svg
@@ -24,41 +30,60 @@ function Icon({ id, open }) {
   );
 }
 
-const RutaCourse = () => {
-    const [open, setOpen] = useState(0);
- 
+const RutaCourse = ( {setVideoId} ) => {
+  const [open, setOpen] = useState(0);
+
   const handleOpen = (value) => {
     setOpen(open === value ? 0 : value);
   };
+
+  const handleClick = (id) =>{
+   setVideoId(id)
+  }
   return (
-    
-    <Fragment className='border-2 border-white'>
-    <Accordion open={open === 1} icon={<Icon id={1} open={open}  />}>
-      <AccordionHeader onClick={() => handleOpen(1)} className='text-2xl'>
-        What is Material Tailwind?
-      </AccordionHeader>
-      <AccordionBody>
-        We&apos;re not always in the position that we want to be at.
-        We&apos;re constantly growing. We&apos;re constantly making mistakes.
-        We&apos;re constantly trying to express ourselves and actualize our
-        dreams.
-      </AccordionBody>
-    </Accordion>
-    <Accordion open={open === 2} icon={<Icon id={2} open={open} />}>
-      <AccordionHeader onClick={() => handleOpen(2)} className='text-2xl'>
-        How to use Material Tailwind?
-      </AccordionHeader>
-      <AccordionBody>
-        We&apos;re not always in the position that we want to be at.
-        We&apos;re constantly growing. We&apos;re constantly making mistakes.
-        We&apos;re constantly trying to express ourselves and actualize our
-        dreams.
-      </AccordionBody>
-    </Accordion>
-    
-  </Fragment>
+    <Fragment className="border-2 border-white">
+      <Accordion open={open === 1} icon={<Icon id={1} open={open} />}>
+        <AccordionHeader onClick={() => handleOpen(1)} className="text-2xl">
+          Introduccion a JavaScript
+        </AccordionHeader>
+        <AccordionBody>
+          {introduccion.map((video) => (
+            <p  onClick={()=>handleClick(video.id)} key={video.id}>{video.titulo}</p>
+          ))}
+        </AccordionBody>
+      </Accordion>
+      <Accordion open={open === 2} icon={<Icon id={2} open={open} />}>
+        <AccordionHeader onClick={() => handleOpen(2)} className="text-2xl">
+          Fundamentos en programacion
+        </AccordionHeader>
+        <AccordionBody>
+          {fundamentos.map((video) => (
+            <p  onClick={()=>handleClick(video.id)} key={video.id}>{video.titulo}</p>
+          ))}
+        </AccordionBody>
+      </Accordion>
+      <Accordion open={open === 3} icon={<Icon id={3} open={open} />}>
+        <AccordionHeader onClick={() => handleOpen(3)} className="text-2xl">
+          Un piano en javaScript
+        </AccordionHeader>
+        <AccordionBody>
+          {piano.map((video) => (
+            <p  onClick={()=>handleClick(video.id)} key={video.id}>{video.titulo}</p>
+          ))}
+        </AccordionBody>
+      </Accordion>
+      <Accordion open={open === 4} icon={<Icon id={4} open={open} />}>
+        <AccordionHeader onClick={() => handleOpen(4)} className="text-2xl">
+          Caracteristicas Nuevas ES6
+        </AccordionHeader>
+        <AccordionBody>
+          {caracteristicas.map((video) => (
+            <p  onClick={()=>handleClick(video.id)} key={video.id}>{video.titulo}</p>
+          ))}
+        </AccordionBody>
+      </Accordion>
+    </Fragment>
+  );
+};
 
-  )
-}
-
-export default RutaCourse
+export default RutaCourse;
